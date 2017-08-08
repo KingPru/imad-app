013715f0cf97beaf65866d0def08b1109bc6f25f
KingPru <king.pru14@gmail.com>
2017-08-08 13:32:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ alert('hi');
 var rt = document.getElementById('main');
 
 rt.innerHTML = "new value";
+
+var img = document.getElementById('fun');
+
+img.onclick = function () {
+    img.style.marginleft = "100px";
+};
