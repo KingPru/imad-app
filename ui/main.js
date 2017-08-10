aa465206075e06b76494a701184f73d79c933c88
KingPru <king.pru14@gmail.com>
2017-08-10 17:28:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,9 @@
 console.log('Loaded!');
 
 
-var rt = document.getElementById('main');
+var button = document.getElementById('counter');
+var counter = 0;
 
-rt.innerHTML = "new value";
-
-var img = document.getElementById('fun');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-};
+counter = counter +1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
